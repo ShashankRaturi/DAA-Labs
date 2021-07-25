c0c4986ced38603cd0357edfa6b5a6e22079b7fd
ShashankRaturi <shashankraturi4@gmail.com>
2021-07-25 15:34:16 +0530

    Inital commit of linear search program

---
+++ Week1/Linear_search.py
@@ -0,0 +1,30 @@
+def linSearch(array , key):
+
+    numOfComparisons  = 0
+    found = False
+
+    for i in range(len(array)):
+        numOfComparisons += 1
+        if key == array[i]:
+            found = True
+            break
+    
+    if found == True:
+        print("Present",numOfComparisons)
+    else:
+        print("Not fpresent" , numOfComparisons)
+
+
+def read():
+    n = int(input())
+
+    for i in range(n):
+        length = int(input())
+        array = list(map(int , input().split()))
+        key = int(input())
+
+        linSearch(array , key)
+
+
+if __name__ == '__main__':
+    read()
